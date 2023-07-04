0761b9a8faecc020125c4f661f7a14b19300b4a8
Evan Chisholm <elchisholm248@gmail.com>
2023-07-03 22:33:15 -0400

    sketched out some functions and types for HNSW, added Readme

--- index.ts
+++ index.ts
@@ -1,13 +1,26 @@
-console.log("hello world")
-
-interface Point {
+interface Point<DataType> {
     embedding: number[];
-    
+    size: number;
+    data: DataType;
 }
 
 interface Layer {
     points: Array<{
-        point: Point,
-        connections: number[]
+        point: number;
+        connections: number[];
     }>;
 }
+
+interface Index<DataType> {
+    layers: Layer[];
+    points: Point<DataType>[];
+}
+
+function constructIndex<DataType>(
+    rawPoints: Point<DataType>[],
+    numberOfLayers: number
+): Index<DataType> {
+    return { layers: [], points: [] };
+}
+
+function probabilityInsert(levelMultiplier: number) {}
