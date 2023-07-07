5b5f782d3a89d9cd7d99029ed582fb9f16e8457b
Evan Chisholm <elchisholm248@gmail.com>
2023-07-06 21:31:39 -0400

    added queue logic for vamana search function, next step is add a stop condition and keep track of most similar nodes to return

--- vamana.ts
+++ vamana.ts
@@ -1,4 +1,4 @@
-import { Point } from "./knn";
+import { Point, calcSimilarity } from "./knn";
 
 type Graph<DataType> = Array<{
     point: Point<DataType>;
@@ -15,16 +15,63 @@ function search<DataType>(
     target: Point<DataType>,
     graph: Graph<DataType>
 ): Array<Point<DataType>> {
+    // visited map, keeps track of which nodes in the graph have and have not been vistited
     const visited: {
         [key: number]: boolean;
     } = {};
-    const q: number[] = [Math.round(Math.random() * (graph.length - 1))];
 
+    // entry point
+    const entryPointIndex = Math.round(Math.random() * (graph.length - 1));
+    const entryPoint = graph[entryPointIndex].point;
+    const entrySimilarity = calcSimilarity(target, entryPoint);
+
+    // setup queue
+    // keeps track of which nodes to search next
+    const q: Array<{
+        index: number;
+        similarity: number;
+        point: Point<DataType>;
+    }> = [
+        {
+            index: entryPointIndex,
+            similarity: entrySimilarity,
+            point: entryPoint,
+        },
+    ];
+
+    // greedy bfs
+    // TODO: keep track of most similar points
+    // TODO: create stop condition
     while (q.length !== 0) {
-        const current: number = q.shift()!;
-        for (const next of graph[current].edges) {
-            if (visited[next]) continue;
-            // TODO: calculate similarity between next node and target node then put into queue accordingly
+        const current = q.shift()!;
+        for (const nextIndex of graph[current.index].edges) {
+            // if this node has been visited skip
+            if (visited[nextIndex]) continue;
+            // mark this node as visited for future runs
+            visited[nextIndex] = true;
+
+            const sim = calcSimilarity(target, graph[nextIndex].point);
+
+            // find spot in q where it belongs so the q is sorted by similarity
+            let isInserted = false;
+            for (const [i, p] of q.entries()) {
+                if (!(sim > p.similarity)) continue;
+
+                q.splice(i, 0, {
+                    point: graph[nextIndex].point,
+                    similarity: sim,
+                    index: nextIndex,
+                });
+                isInserted = true;
+                break;
+            }
+            // if it wasn't inserted in the above search because it's similarity is lower than everything else append to the end
+            if (!isInserted)
+                q.push({
+                    point: graph[nextIndex].point,
+                    similarity: sim,
+                    index: nextIndex,
+                });
         }
     }
 
