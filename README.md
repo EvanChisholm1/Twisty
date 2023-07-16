7dc887e0901f913e4283ee87a5ed903e5ba89ccd
Evan Chisholm <elchisholm248@gmail.com>
2023-07-15 21:15:46 -0400

    added average similarity to the benchmarks

--- README.md
+++ README.md
@@ -8,17 +8,19 @@ Here are some benchmarks of the various algorithms implemented running on my com
 
 ### Benchmark on Small Dataset of 1000 Arxiv Titles
 
-| Algorithm      | Average Time Over 5 Runs |
-| -------------- | ------------------------ |
-| KNN            | 2.4058ms                 |
-| DiskANN/Vamana | 0.822ms                  |
+| Algorithm      | Average Time Over 5 Runs | Average Similarity |
+| -------------- | ------------------------ | ------------------ |
+| KNN            | 2.4058ms                 | 0.7422             |
+| DiskANN/Vamana | 0.822ms                  | 0.6952             |
 
 ### Benchmark on Dataset of 10k Arxiv Titles
 
-| Algorithm      | Average Time Over 5 Runs |
-| -------------- | ------------------------ |
-| KNN            | 24.35                    |
-| DiskANN/Vamana | 1.25                     |
+| Algorithm      | Average Time Over 5 Runs | Average Similarity |
+| -------------- | ------------------------ | ------------------ |
+| KNN            | 24.35                    | 0.78201            |
+| DiskANN/Vamana | 1.25                     | 0.68904            |
+
+Keep in mind the similarity scores for DiskANN are currently with a rather low amount of connections per node at 5 and no fancy graph optimizations yet.
 
 Eventually I will have multiple algorithims, specifically Annoy, and HNSW, as well as comparisons to other popular libraries and projects. The goal is to become the fastest JavaScript/TypeScript based vector database.
 
--- benchmark.ts
+++ benchmark.ts
@@ -2,11 +2,26 @@ import { readFileSync } from "fs";
 import { Point, knn } from "./knn";
 import { constructGraph, search } from "./vamana";
 
-const file: string = readFileSync("./arxiv-titles.json", "utf-8")!;
+console.log("loading file...");
+// const file: string = readFileSync("./arxiv-titles.json", "utf-8")!;
+const file: string = readFileSync("./arxiv-titles-10k.json", "utf-8")!;
 const embeddings: Point<string>[] = JSON.parse(file);
+
+// let embeddings: Point<string>[] = [];
+// for (let i = 0; i < 10; i++) {
+//     const file: string = readFileSync(
+//         `./arxiv-titles-100k-${i}.json`,
+//         "utf-8"
+//     )!;
+//     const jsonEmbeddings: Point<string>[] = JSON.parse(file);
+//     embeddings = [...embeddings, ...jsonEmbeddings];
+// }
+
+console.log("File Loaded!", "\n");
 
 // KNN brute force benchmark
 const timesKNN: number[] = [];
+const KNNAvgSimilarities: number[] = [];
 
 const globalStart = Date.now();
 for (let i = 0; i < 1000; i++) {
@@ -17,17 +32,27 @@ for (let i = 0; i < 1000; i++) {
     const end = Date.now();
 
     timesKNN.push(end - start);
+
+    const avgSimilarity =
+        mostSimilar.reduce((acc, cur) => acc + cur.similarity, 0) / 10;
+    KNNAvgSimilarities.push(avgSimilarity);
 }
 
 const globalEnd = Date.now();
-const totalTime = globalEnd - globalStart;
 
+const totalTime = globalEnd - globalStart;
 const averageTimeMs = timesKNN.reduce((acc, cur) => acc + cur) / 1000;
+const KNNAvgSimilartiy =
+    KNNAvgSimilarities.reduce((acc, cur) => acc + cur) / 1000;
+
 console.log("KNN average time", averageTimeMs);
 console.log("KNN total time", totalTime);
+console.log("KNN average similarity", KNNAvgSimilartiy, "\n");
 
 // diskANN/vamana benchmark
 const timesDiskANN: number[] = [];
+const diskANNSimilarities: number[] = [];
+
 for (let i = 0; i < 1000; i++) {
     const embeddingIndex = Math.round(Math.random() * 999);
 
@@ -40,10 +65,18 @@ for (let i = 0; i < 1000; i++) {
     // console.log(end - start);
 
     timesDiskANN.push(end - start);
+
+    const avgSimilarity =
+        mostSimilar.reduce((acc, cur) => acc + cur.similarity, 0) / 10;
+    diskANNSimilarities.push(avgSimilarity);
 }
 
-const diskANNTotalTimeMs = timesDiskANN.reduce((acc, cur) => acc + cur);
-const diskANNAvgTimeMs = diskANNTotalTimeMs / 1000;
+const diskANNAvgSimilarity =
+    diskANNSimilarities.reduce((acc, cur) => acc + cur) / 1000;
+
+const diskANNTotalTimeMs = timesDiskANN.reduce((acc, cur) => acc + cur) / 1000;
+const diskANNAvgTimeMs = diskANNTotalTimeMs;
 
 console.log("DiskANN average time:", diskANNAvgTimeMs);
 console.log("DiskANN Total Time:", diskANNTotalTimeMs);
+console.log("DiskANN average similarity:", diskANNAvgSimilarity);
